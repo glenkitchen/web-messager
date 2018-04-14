c37b79881f5b119265ffadf99902ae8ac6ac8769
glenkitchen <glen.kitchen@gmail.com>
2018-04-14 16:51:09 +0200

    commit

---
+++ test-harness/js/host.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    
+    $('#showIframeForm').click(function () {
+        $('#iframeFormContainer').toggle();
+    });
+
+    $('#showMessageForm').click(function () {
+        $('#messageFormContainer').toggle();
+    });
+
+    $('#showMessages').click(function () {
+        $('#messagesContainer').toggle();
+    });
+
+});
